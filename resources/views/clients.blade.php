45aacc63f1597d7eebf9e7e927a3753c494ef8b8
FREDERIC <kannif@ymail.com>
2020-02-24 22:19:19 +0400

    us01 front end

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -2,12 +2,35 @@
 
 
 @section('contenu')
-   liste des clients
+<h1 class="text-center m-5 "> liste des clients</h1>
+  
   {{ csrf_token() }}
 
+
+  <div class="container">
+<div class="row">
+  
    @foreach ($clients as $client )
-   <p>{{$client-> nom}}</p>
+ 
+   <div class="col-md-3">
+      <a href="/clients/{{$client-> id}} " >
+         <div class="card m-3" style="width: 15rem; height:10vh">
+            <h5 class="card-title text-center my-auto client">   {{$client-> prenom}}    {{$client-> nom}}</h5>
+         </div>
+      </a>
+   </div>
+
    @endforeach
+
+
+</div>
+
+  </div>
+
+
+  
+
+
 @endsection
 
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,11 @@
         <!-- css bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
        
-   
+   <style>
+   .client{
+       color: black;
+   }
+   </style>
 
 
 
